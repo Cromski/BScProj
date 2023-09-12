13d74398102d22127d670f9961e981072cb1cb4b
Cromski <jakobrhenriksen@outlook.com>
2023-09-12 23:47:56 +0200

    added more behaviors

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 behaviors = json.load(open('behaviors.json', 'r'))
 
-promptChatGPT(0.3, behaviors["Behaviors"][1]["act"], behaviors["Behaviors"][1]["keywords"], "iPhone 15")
+promptChatGPT(0.8, behaviors["Behaviors"][3]["act"], behaviors["Behaviors"][3]["keywords"], "iPhone 15")
 
 
 
