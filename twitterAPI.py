4ed28895bd60b9388134d7d7e0c5c370e1807db4
Cromski <jakobrhenriksen@outlook.com>
2023-09-19 15:16:47 +0200

    made the behavior variable to loop over all accounts in main

--- twitterAPI.py
+++ twitterAPI.py
@@ -7,13 +7,14 @@ load_dotenv()
 
 
 # credits for twitter authorization: https://github.com/loonathedorm/Twitter-Tracery-Bot/tree/main
-def init_twitter_client():
+def init_twitter_client(behavior):
     """Initialising Twitter API Client"""
     # Getting Twitter API Keys
-    consumer_key = os.getenv("CONTROVERSIAL_X_API_KEY")
-    consumer_secret = os.getenv("CONTROVERSIAL_X_API_KEY_SECRET")
-    access_token = os.getenv("CONTROVERSIAL_X_ACCESS_TOKEN")
-    access_token_secret = os.getenv("CONTROVERSIAL_X_ACCESS_TOKEN_SECRET")
+    behavior = behavior.upper()
+    consumer_key = os.getenv(f"{behavior}_X_API_KEY")
+    consumer_secret = os.getenv(f"{behavior}_X_API_KEY_SECRET")
+    access_token = os.getenv(f"{behavior}_X_ACCESS_TOKEN")
+    access_token_secret = os.getenv(f"{behavior}_X_ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
@@ -28,6 +29,6 @@ def post_to_twitter(api_v2,message):
     tweet = api_v2.create_tweet(text=message)
     print(Fore.GREEN + f'\n####---> Posted: ID={tweet[0]["id"]}, QUOTE={message}' + Style.RESET_ALL)
 
-def post_msg_to_twitter(message):
-    api_v2 = init_twitter_client()[1]
+def post_msg_to_twitter(behavior, message):
+    api_v2 = init_twitter_client(behavior)[1]
     post_to_twitter(api_v2,message)
