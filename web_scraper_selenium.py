e09718432ecb8cee860d780d23068c0c286f0369
Cromski <jakobrhenriksen@outlook.com>
2023-09-20 12:11:34 +0200

    Web scraper now prints all trends

--- web_scraper_selenium.py
+++ web_scraper_selenium.py
@@ -1,21 +1,36 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+import os
+from selenium.webdriver.common.action_chains import ActionChains
+
+load_dotenv()
 
 url = 'https://www.twitter.com/i/trends'
 driver = webdriver.Chrome()
 driver.get(url)
 
-mail = "x"
-pw = "x"
-phone = "x"
+username = os.getenv("PESSIMISTIC_USERNAME")
+pw = os.getenv("PESSIMISTIC_PW")
 
-email = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')))
-email.click()
-email.send_keys(mail)
-email.send_keys(Keys.RETURN)
+username_input = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')))
+username_input.click()
+username_input.send_keys(username)
+username_input.send_keys(Keys.RETURN)
+
+time.sleep(1)
+actions = ActionChains(driver)
+actions.send_keys(pw + Keys.RETURN)
+actions.perform()
+
+time.sleep(3)
+page_content = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div').text
+
+print(page_content)
 
 while True:
     pass
