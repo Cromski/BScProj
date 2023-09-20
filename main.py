f7a54f589b5b7cb659dd0eec36e742c70087579f
Cromski <jakobrhenriksen@outlook.com>
2023-09-21 01:49:02 +0200

    Will run forever and post each 1.5 hrs with a new topic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from chatGPTprompts import promptChatGPT
 from twitterAPI import post_msg_to_twitter
 from web_scraper_selenium import get_twitter_trends
@@ -6,13 +7,17 @@ import json
 behaviors = json.load(open('behaviors.json', 'r'))
 
 temp = 0.8
-topic = get_twitter_trends()[0][1] # get the most trending topic
 #post to single behavior
-# behaviorVal = 0
-# tweet = promptChatGPT(0.8, behaviors["Behaviors"][behaviorVal]["act"], behaviors["Behaviors"][behaviorVal]["keywords"], "#IDontWantToOverreactBUT")
+# topic = get_twitter_trends()[0][1] # get the most trending topic
+# tweet = promptChatGPT(0.8, behaviors["Behaviors"][behaviorVal]["act"], behaviors["Behaviors"][behaviorVal]["keywords"], topic)
 # post_msg_to_twitter(behaviors["Behaviors"][behaviorVal]["act"], tweet)
 
-#post to all behaviors
-for i in range(4):
-    tweet = promptChatGPT(temp, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], topic)
-    post_msg_to_twitter(behaviors["Behaviors"][i]["act"], tweet)
+#post to all behaviors each 1.5 hours
+while True:
+    topic = get_twitter_trends()[0][1] # get the most trending topic
+
+    #post to all behaviors
+    for i in range(4):
+        tweet = promptChatGPT(temp, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], topic)
+        post_msg_to_twitter(behaviors["Behaviors"][i]["act"], tweet)
+    time.sleep(60*60*1.5) # wait 1.5 hours
