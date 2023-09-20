9b0bf886ecab19229fc4b60efd1c0972e055957b
Cromski <jakobrhenriksen@outlook.com>
2023-09-21 01:37:52 +0200

    topic is now fetched from twitter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from chatGPTprompts import promptChatGPT
 from twitterAPI import post_msg_to_twitter
+from web_scraper_selenium import get_twitter_trends
 import json
 
 behaviors = json.load(open('behaviors.json', 'r'))
 
+temp = 0.8
+topic = get_twitter_trends()[0][1] # get the most trending topic
 #post to single behavior
 # behaviorVal = 0
 # tweet = promptChatGPT(0.8, behaviors["Behaviors"][behaviorVal]["act"], behaviors["Behaviors"][behaviorVal]["keywords"], "#IDontWantToOverreactBUT")
@@ -11,5 +14,5 @@ behaviors = json.load(open('behaviors.json', 'r'))
 
 #post to all behaviors
 for i in range(4):
-    tweet = promptChatGPT(0.8, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], "Lazio")
+    tweet = promptChatGPT(temp, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], topic)
     post_msg_to_twitter(behaviors["Behaviors"][i]["act"], tweet)
