f1c90d8742bc38b511be88dd7a9267fbaca3446f
Cromski <jakobrhenriksen@outlook.com>
2023-09-20 12:24:34 +0200

    simplified the authorization

--- twitterAPI.py
+++ twitterAPI.py
@@ -5,11 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-# credits for twitter authorization: https://github.com/loonathedorm/Twitter-Tracery-Bot/tree/main
 def init_twitter_client(behavior):
-    """Initialising Twitter API Client"""
-    # Getting Twitter API Keys
     behavior = behavior.upper()
     consumer_key = os.getenv(f"{behavior}_X_API_KEY")
     consumer_secret = os.getenv(f"{behavior}_X_API_KEY_SECRET")
@@ -18,17 +14,16 @@ def init_twitter_client(behavior):
 
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
-    api_v1 = tweepy.API(auth)
     api_v2 = tweepy.Client(consumer_key=consumer_key,
                         consumer_secret=consumer_secret,
                         access_token=access_token,
                         access_token_secret=access_token_secret)
-    return api_v1, api_v2
+    return api_v2
 
 def post_to_twitter(api_v2,message):
     tweet = api_v2.create_tweet(text=message)
     print(Fore.GREEN + f'\n####---> Posted: ID={tweet[0]["id"]}, QUOTE={message}' + Style.RESET_ALL)
 
 def post_msg_to_twitter(behavior, message):
-    api_v2 = init_twitter_client(behavior)[1]
+    api_v2 = init_twitter_client(behavior)
     post_to_twitter(api_v2,message)
