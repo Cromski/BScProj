f807e0261ba3bc4f1b5906857cf9c24541929c86
Cromski <jakobrhenriksen@outlook.com>
2023-10-15 00:29:47 +0200

    saving all data in data-overview.json function

---
+++ get_overview_of_all_data.py
@@ -0,0 +1,39 @@
+import json
+
+behaviors = json.load(open('behaviors.json', 'r'))
+
+all_results = {}
+
+def get_overview_of_one_behavior(behavior):
+
+    with open(f'tweets-with-analytics/{behavior}-data.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    total_values = {
+        "likes": [],
+        "retweets": [],
+        "comments": [],
+        "impressions": [],
+        "engagements": [],
+        "detail_expands": [],
+        "new_followers": [],
+        "profile_visits": []
+    }
+
+    for item in data:
+        if item["checked"] == "true":
+            for key in total_values:
+                total_values[key].append(int(item[key]))
+
+    total_values["total_tweets"] = len(data)
+
+    return total_values
+
+def get_overview_of_all_behaviors():
+    for i in range(4):
+        behavior_name = behaviors["Behaviors"][i]["act"]
+        behavior_data = get_overview_of_one_behavior(behavior_name)
+        all_results[behavior_name] = behavior_data
+
+    with open('data-overview/data-overview.json', 'w') as output_json:
+        json.dump(all_results, output_json, indent=4)
