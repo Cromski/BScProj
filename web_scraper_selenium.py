535faedb9b4e56c384fc4d87d0f686a9fc477c14
Cromski <jakobrhenriksen@outlook.com>
2023-10-15 10:48:29 +0200

    optimised trending scraper

--- web_scraper_selenium.py
+++ web_scraper_selenium.py
@@ -7,22 +7,30 @@ from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 import os
 from selenium.webdriver.common.action_chains import ActionChains
+import re
 
 load_dotenv()
 
 def convert_string_nr_to_int(str):
     s = str.replace(',','').replace(' posts', '')
     return float(s.replace('K',''))*1000 if (s.__contains__('K')) else float(s.replace('M',''))*1000000 if (s.__contains__('M')) else float(s)
-    
+
 def reorder_scraping_result(text):
-    text = text.split('\n')
-    temp = []
+    pattern = r'^\d+\n·'
+    text = re.split(pattern, text, flags=re.MULTILINE)
+    text = [segment.strip() for segment in text if segment.strip()]
     for i in range(0, len(text)):
-        if (i+1) % 3 == 0 and i != 0:
-            if text[i][0].isdigit() : # and text[i][1].isdigit()
-                temp.append((text[i-2], text[i-1], convert_string_nr_to_int(text[i])))
-    temp.sort(key=lambda x: x[2], reverse=True)
-    return temp
+        short_data = text[i].split('\n')
+        if len(short_data) == 3:
+            if short_data[2].__contains__('posts'):
+                text[i] = (short_data[0], short_data[1], convert_string_nr_to_int(short_data[2]))
+            else:
+                text[i] = (short_data[0], short_data[1], 0.0)
+        else:
+            text[i] = (short_data[0], short_data[1], 0.0)
+
+    return sorted(text, key=lambda x: float(x[-1]), reverse=True)
+
 
 def get_twitter_trends():
     url = 'https://www.twitter.com/i/trends'
@@ -48,6 +56,6 @@ def get_twitter_trends():
 
     time.sleep(3)
     page_content = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/section/div').text
-    
+
     driver.quit()
     return reorder_scraping_result(page_content)
