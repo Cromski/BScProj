1211826e000e19623822025130d6214789cea13c
Cromski <jakobrhenriksen@outlook.com>
2023-10-19 12:47:30 +0200

    made 2 seperate graphs for impressions attribute, and added the mean to each graph

--- create_boxplots.py
+++ create_boxplots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import json
 import pandas as pd
 
-def create_boxplot(attribute, data_overview):
+def create_boxplot(attribute, data_overview, show_outliers=True):
 
     data = {}
 
@@ -21,18 +21,39 @@ def create_boxplot(attribute, data_overview):
     plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
     sns.set(style="whitegrid")  # Set the style
 
-    # Specify the desired y-axis ticks
-    y_ticks = [i for i in range(0, max(behavior_data[attribute.capitalize()]) + 1, 5)]
-
-    sns.boxplot(x="Behavior", y=attribute.capitalize(), data=behavior_data, palette="Set3", hue="Behavior")
+    if show_outliers:
+        # y_ticks = [i for i in range(0, max(behavior_data[attribute.capitalize()]) + 1, 5)]
+        sns.boxplot(x="Behavior", 
+                    y=attribute.capitalize(), 
+                    data=behavior_data, 
+                    palette="Set3", 
+                    hue="Behavior", 
+                    showfliers=True, 
+                    showmeans=True,
+                    meanprops={"marker": "+",
+                       "markeredgecolor": "green",
+                       "markersize": "10"})
+    else:
+        sns.boxplot(x="Behavior", 
+                    y=attribute.capitalize(), 
+                    data=behavior_data, 
+                    palette="Set3", 
+                    hue="Behavior", 
+                    showfliers=False, 
+                    showmeans=True,
+                    meanprops={"marker": "+",
+                       "markeredgecolor": "green",
+                       "markersize": "10"})
     plt.title(f"Boxplot of {attribute.capitalize()} by Behavior")
     plt.xlabel("Behavior")
     plt.ylabel(attribute.capitalize())
 
-    # Set the y-axis ticks
-    plt.yticks(y_ticks)
-
-    plt.savefig(f'data-overview/{attribute}-boxplot.png')
+    if show_outliers:
+        # plt.yticks(y_ticks)
+        plt.savefig(f'data-overview/{attribute}-boxplot.png')
+    else:
+        plt.savefig(f'data-overview/{attribute}-boxplot-without-outliers.png')
+        create_boxplot(attribute, data_overview, show_outliers=True)
     plt.close('all')
 
 def create_all_boxplots():
@@ -42,4 +63,9 @@ def create_all_boxplots():
     all_attributes = ['likes', 'retweets', 'comments', 'impressions', 'engagements', 'detail_expands', 'new_followers', 'profile_visits']
     
     for attr in all_attributes:
-        create_boxplot(attr, data_overview)
+        if attr == "impressions":
+            create_boxplot(attr, data_overview, show_outliers=False)
+        else:
+            create_boxplot(attr, data_overview)
+
+create_all_boxplots()
