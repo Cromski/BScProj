98037dbeae669708f1cf758acd3f298d7db4dc73
Cromski <jakobrhenriksen@outlook.com>
2023-11-13 15:20:54 +0100

    pipeline now rewrites most popular tweet in trending topic and inserts the same img src from the tweet

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time
 from datetime import datetime
 from chatGPTprompts import promptChatGPT
 from twitterAPI import post_msg_to_twitter
-from web_scraper_selenium import get_twitter_trends
+# from web_scraper_selenium import get_twitter_trends
+from fetch_popular_tweet_by_trend_scraper import get_twitter_trends
 from add_data_to_json import add_tweet_obj, edit_tweet_analytics, get_list_of_tweets_older_than_a_day
 from web_scrape_analysis import get_analysis_of_tweet
 from get_overview_of_all_data import get_overview_of_all_behaviors
@@ -35,20 +36,21 @@ while True:
         if len(tweets_to_get_updated) > 0: print(Fore.CYAN + f'\n####---> Data updated from tweets ({behaviors["Behaviors"][i]["act"]}): ' + str(tweets_to_get_updated) + Style.RESET_ALL)
 
 
-    topic = get_twitter_trends()[0][1] # get the most trending topic
+    popular_tweet,popular_tweet_src = get_twitter_trends() # get the most trending topic
 
-    print(Fore.CYAN + f'\n####---> Topic chosen: {topic}' + Style.RESET_ALL)
+    print(Fore.CYAN + f'\n####---> Topic chosen: {popular_tweet}' + Style.RESET_ALL)
     # post to all behaviors
     for i in range(4):
 
-        prompt,tweet = promptChatGPT(temp, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], topic)
-        tweet_id = post_msg_to_twitter(behaviors["Behaviors"][i]["act"], tweet)
-        add_tweet_obj(behaviors["Behaviors"][i]["act"], str(tweet_id), prompt, tweet)
+        prompt,tweet = promptChatGPT(temp, behaviors["Behaviors"][i]["act"], behaviors["Behaviors"][i]["keywords"], popular_tweet)
+        tweet_id = post_msg_to_twitter(behaviors["Behaviors"][i]["act"], tweet,popular_tweet_src)
+        add_tweet_obj(behaviors["Behaviors"][i]["act"], str(tweet_id), prompt, tweet, popular_tweet_src)
         
     print(Fore.CYAN + f'\n####---> All tweets posted' + Style.RESET_ALL)
 
     get_overview_of_all_behaviors()
     create_all_boxplots()
+
     print(Fore.CYAN + f'\n####---> All boxplots created' + Style.RESET_ALL)
 
     print(Fore.BLUE + f"##### round {roundnr} done @ {datetime.now().strftime('%H:%M:%S')} #####\n" + Style.RESET_ALL)
