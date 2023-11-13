bcf44b5905599c24d06cd2b7cf31df895218f6ae
Cromski <jakobrhenriksen@outlook.com>
2023-11-13 15:21:16 +0100

    pipeline now rewrites most popular tweet in trending topic and inserts  the img src

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,6 +1,9 @@
 import os
 import tweepy
+import io
+import requests
 from colorama import Fore, Style
+import tempfile
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,17 +17,38 @@ def init_twitter_client(behavior):
 
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
+    api_v1 = tweepy.API(auth)
     api_v2 = tweepy.Client(consumer_key=consumer_key,
                         consumer_secret=consumer_secret,
                         access_token=access_token,
                         access_token_secret=access_token_secret)
-    return api_v2
+    return api_v1,api_v2
 
-def post_to_twitter(api_v2,message,behavior):
-    tweet = api_v2.create_tweet(text=message)
-    print(Fore.GREEN + f'\n####---> Posted: Behavior={behavior} ID={tweet[0]["id"]}, QUOTE={message}' + Style.RESET_ALL)
+def post_to_twitter(api_v1,api_v2, message, behavior, image_url=None):
+    
+    # Create the tweet with the optional image
+    # add image_url to the media_ids keyword argument if you want to attach media to any of the statuses you’re posting.
+    response = requests.get(image_url)
+    image_content = response.content
+
+    # Save the image content to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(image_content)
+        temp_file_path = temp_file.name
+
+    try:
+        # Upload the image using api_v2.media.upload
+        media = api_v1.media_upload(temp_file_path)
+        media_id = media.media_id
+        tweet = api_v2.create_tweet(text=message, media_ids=[media_id])
+        print(Fore.GREEN + f'\n####---> Posted: Behavior={behavior} ID={tweet[0]["id"]}, QUOTE={message}' + Style.RESET_ALL)
+    finally:
+        # Clean up the temporary file
+        os.remove(temp_file_path)
+    
+    
     return tweet[0]["id"]
 
-def post_msg_to_twitter(behavior, message):
-    api_v2 = init_twitter_client(behavior)
-    return post_to_twitter(api_v2,message,behavior)
+def post_msg_to_twitter(behavior, message, image_url=None):
+    api_v1,api_v2 = init_twitter_client(behavior)
+    return post_to_twitter(api_v1,api_v2,message,behavior, image_url=image_url)
